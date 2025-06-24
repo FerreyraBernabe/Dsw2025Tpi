584d3698377ddb71728fe53175abd158b15c690d
Astrid Sant <astridsantbobillo@gmail.com>
2025-06-24 20:50:58 -0300

    FIX: Modificación de atributos de la clase producto en Context y la informacion del archivo json

--- Dsw2025Tpi.Data/Dsw2025TpiContext.cs
+++ Dsw2025Tpi.Data/Dsw2025TpiContext.cs
@@ -16,10 +16,11 @@ public class Dsw2025TpiContext : DbContext
             entity.ToTable("Products");
             entity.Property(e => e.Id).IsRequired();
             entity.Property(e => e.Sku).IsRequired().HasMaxLength(6);
-            entity.Property(e => e.Name).IsRequired().HasMaxLength(50);
-            entity.Property(e => e.Price).HasColumnType("decimal(15,2)");
-            entity.Property(e => e.Stock).IsRequired().HasMaxLength(3);
             entity.Property(e => e.InternalCode).IsRequired().HasMaxLength(7);
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.Descripcion).HasMaxLength(200);
+            entity.Property(e => e.CurrentUnitPrice).HasColumnType("decimal(15,2)");
+            entity.Property(e => e.StockQuantity).IsRequired().HasMaxLength(3);
             entity.Property(e => e.IsActive).IsRequired().HasDefaultValue(true);
         });
 
@@ -30,8 +31,8 @@ public class Dsw2025TpiContext : DbContext
             entity.Property(e => e.Name).IsRequired().HasMaxLength(50);
             entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
             entity.Property(e => e.PhoneNumber).IsRequired().HasMaxLength(15);
-                   
-        })            ;
+
+        });
 
         modelBuilder.Entity<Order>(entity =>
         {
@@ -42,9 +43,9 @@ public class Dsw2025TpiContext : DbContext
             entity.Property(e => e.BillingAddress).IsRequired().HasMaxLength(200);
             entity.Property(e => e.Notes).HasMaxLength(500);
             entity.Property(e => e.TotalAmount).HasColumnType("decimal(15,2)");
-            /*entity.HasOne(o => o.Customer)
+            entity.HasOne(o => o.Customer)
                 .WithMany(c => c.Orders)
-                .HasForeignKey(o => o.CustomerId);*/
+                .HasForeignKey(o => o.CustomerId);
         });
 
         modelBuilder.Entity<OrderItem>(entity =>
@@ -53,13 +54,13 @@ public class Dsw2025TpiContext : DbContext
             entity.Property(e => e.Id).IsRequired();
             entity.Property(e => e.Quantity).IsRequired().HasDefaultValue(5);
             entity.Property(e => e.Price).HasColumnType("decimal(15,2)");
-          /*  entity.HasOne(oi => oi.Order)
-                .WithMany(o => o.OrderItems)
-                .HasForeignKey(oi => oi.OrderId);
+            entity.HasOne(oi => oi.Order)
+                 .WithMany(o => o.OrderItems)
+                 .HasForeignKey(oi => oi.OrderId);
             entity.HasOne(oi => oi.Product)
                 .WithMany()
-                .HasForeignKey(oi => oi.ProductId);*/
-        }); 
+                .HasForeignKey(oi => oi.ProductId);
+        });
 
 
     }
