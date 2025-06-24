dd43d162ba0b5a58009fc5cb9d6ab65d152794f1
Astrid Sant <astridsantbobillo@gmail.com>
2025-06-24 20:53:02 -0300

    FIX: Modificación de atributos de la clase producto

--- Dsw2025Tpi.Domain/Entities/Product.cs
+++ Dsw2025Tpi.Domain/Entities/Product.cs
@@ -4,54 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dsw2025Tpi.Domain.Entities
+namespace Dsw2025Tpi.Domain.Entities;
+public class Product : EntityBase
 {
-    public class Product : EntityBase
+    public Product() { }
+
+    public Product(string sku, string internalCode, string name, string descripcion, decimal currentUnitPrice, int stockQuantity)
     {
-        public Product() { }
-
-        public Product(string sku, string name, decimal price, int stock, string internalCode)
-        {
-            Id = Guid.NewGuid();
-            Sku = sku;
-            Name = name;
-            Price = price;
-            Stock = stock;
-            InternalCode = internalCode;
-            IsActive = true;
-        }
-
-        public string Sku { get; set; }
-        public string Name { get; set; }
-
-        private decimal _price;
-        public decimal Price
-        {
-            get => _price;
-            set
-            {
-                if (value <= 0)
-                    throw new ArgumentException("El precio debe ser mayor a 0.");
-                _price = value;
-            }
-        }
-
-        private int _stock;
-        public int Stock
-        {
-            get => _stock;
-            set
-            {
-                if (value < 0)
-                    throw new ArgumentException("La cantidad de stock no puede ser negativa.");
-                _stock = value;
-            }
-        }
-
-        public string InternalCode { get; set; }
-        public bool IsActive { get; set; }
-
-        public ICollection<OrderItem>? Items { get; set; }
+        Id = Guid.NewGuid();
+        Sku = sku;
+        InternalCode = internalCode;
+        Name = name;
+        Descripcion = descripcion;
+        CurrentUnitPrice = currentUnitPrice;
+        StockQuantity = stockQuantity;
+        IsActive = true;
     }
 
+    public string Sku { get; set; }
+    public string Name { get; set; }
+
+    private decimal _currentUnitPrice;
+    public decimal CurrentUnitPrice
+    {
+        get => _currentUnitPrice;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("El precio debe ser mayor a 0.");
+            _currentUnitPrice = value;
+        }
+    }
+
+    private int _stockQuantity;
+    public int StockQuantity
+    {
+        get => _stockQuantity;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("La cantidad de stock no puede ser negativa.");
+            _stockQuantity = value;
+        }
+    }
+
+    public string InternalCode { get; set; }
+
+    public string? Descripcion { get; set; }
+    public bool IsActive { get; set; }
+
+    public ICollection<OrderItem>? Items { get; set; }
 }
