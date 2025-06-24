37b7531dd988fbdc044be6e8c63cc7f59a97bdd7
Astrid Sant <astridsantbobillo@gmail.com>
2025-06-24 09:54:53 -0300

    ADD: Agregación de InMemory en Repositories, creación de helpers, creación de archivos json.

--- Dsw2025Tpi.Data/Dsw2025TpiContext.cs
+++ Dsw2025Tpi.Data/Dsw2025TpiContext.cs
@@ -1,7 +1,66 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dsw2025Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dsw2025Tpi.Data;
 
-public class Dsw2025TpiContext: DbContext
+public class Dsw2025TpiContext : DbContext
 {
+    public Dsw2025TpiContext(DbContextOptions<Dsw2025TpiContext> options) : base(options) { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable("Products");
+            entity.Property(e => e.Id).IsRequired();
+            entity.Property(e => e.Sku).IsRequired().HasMaxLength(6);
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.Price).HasColumnType("decimal(15,2)");
+            entity.Property(e => e.Stock).IsRequired().HasMaxLength(3);
+            entity.Property(e => e.InternalCode).IsRequired().HasMaxLength(7);
+            entity.Property(e => e.IsActive).IsRequired().HasDefaultValue(true);
+        });
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.ToTable("Customers");
+            entity.Property(e => e.Id).IsRequired();
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.PhoneNumber).IsRequired().HasMaxLength(15);
+                   
+        })            ;
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.ToTable("Orders");
+            entity.Property(e => e.Id).IsRequired();
+            entity.Property(e => e.Date).IsRequired();
+            entity.Property(e => e.ShippingAddress).IsRequired().HasMaxLength(200);
+            entity.Property(e => e.BillingAddress).IsRequired().HasMaxLength(200);
+            entity.Property(e => e.Notes).HasMaxLength(500);
+            entity.Property(e => e.TotalAmount).HasColumnType("decimal(15,2)");
+            /*entity.HasOne(o => o.Customer)
+                .WithMany(c => c.Orders)
+                .HasForeignKey(o => o.CustomerId);*/
+        });
+
+        modelBuilder.Entity<OrderItem>(entity =>
+        {
+            entity.ToTable("OrderItems");
+            entity.Property(e => e.Id).IsRequired();
+            entity.Property(e => e.Quantity).IsRequired().HasDefaultValue(5);
+            entity.Property(e => e.Price).HasColumnType("decimal(15,2)");
+          /*  entity.HasOne(oi => oi.Order)
+                .WithMany(o => o.OrderItems)
+                .HasForeignKey(oi => oi.OrderId);
+            entity.HasOne(oi => oi.Product)
+                .WithMany()
+                .HasForeignKey(oi => oi.ProductId);*/
+        }); 
+
+
+    }
 }
