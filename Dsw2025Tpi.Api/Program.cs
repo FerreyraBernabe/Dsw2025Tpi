38ff057803b0de8adb7310fafcc0e4c57c05fc45
Astrid Sant <astridsantbobillo@gmail.com>
2025-06-24 10:11:14 -0300

    ADD: Agregación de controlador de producto, CHANGE: modificación de program

--- Dsw2025Tpi.Api/Program.cs
+++ Dsw2025Tpi.Api/Program.cs
@@ -1,4 +1,8 @@
 
+using Dsw2025Tpi.Application.Services;
+using Dsw2025Tpi.Data.Repositories;
+using Dsw2025Tpi.Domain.Interfaces;
+
 namespace Dsw2025Tpi.Api;
 
 public class Program
@@ -7,13 +11,25 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", policy =>
+            {
+                policy.AllowAnyOrigin()
+                      .AllowAnyHeader()
+                      .AllowAnyMethod();
+            });
+        });
 
+        // Add services to the container.
+       
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         builder.Services.AddHealthChecks();
+        builder.Services.AddSingleton<IRepository, InMemory>();
+        builder.Services.AddTransient<ProductsManagementService>();
 
         var app = builder.Build();
 
@@ -27,6 +43,8 @@ public class Program
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
+
+        app.UseCors("AllowAll");
 
         app.MapControllers();
         
